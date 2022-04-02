36111d32e0cf96610c68312c6063d4343120a241
Kazuki1450 <kzk.08.12.co.jp@gmail.com>
2022-04-02 13:36:57 +0900

    feat: 完成

---
+++ chapter2/item13.py
@@ -0,0 +1,14 @@
+# 13:atch-allアンパック
+
+a = [1, 2, 3, 4, 5]
+
+# NG例。off-by-oneエラー(境界条件に関するエラー)を誘発しうる
+# 例えば「境界値を1ヶ所修正し忘れた」とかが起こりうる
+first = a[0]
+second = a[1]
+others = a[2:]
+print(first, second, others)
+
+# OK例。「*hoge」で「それ以外全部」を受け取れる(=catch-all unpack)
+first, second, *others = a
+print(first, second, others)
